d57ab87216a1d0c682cd01de67a10a813c3ecc15
Ravi <ravi@example.com>
2024-11-06 17:28:03 -0300

    fix: naming conventions

--- src/main/java/br/com/cadastroalunos/service/AlunoService.java
+++ src/main/java/br/com/cadastroalunos/service/AlunoService.java
@@ -42,14 +42,12 @@ public class AlunoService {
 
         if (oldAluno.isPresent()) {
             Aluno newAluno = oldAluno.get();
-
-            newAluno.setNome_aluno(aluno.getNome_aluno());
-            newAluno.setCpf_aluno(aluno.getCpf_aluno());
-            newAluno.setDta_nasc(aluno.getDta_nasc());
-            newAluno.setId_turma(aluno.getId_turma());
-            newAluno.setTelefone_aluno(aluno.getTelefone_aluno());
-            newAluno.setEmail_aluno(aluno.getEmail_aluno());
-
+            newAluno.setNomeAluno(aluno.getNomeAluno());
+            newAluno.setCpfAluno(aluno.getCpfAluno());
+            newAluno.setDataNasc(aluno.getDataNasc());
+            newAluno.setIdTurma(aluno.getIdTurma());
+            newAluno.setTelefoneAluno(aluno.getTelefoneAluno());
+            newAluno.setEmailAluno(aluno.getEmailAluno());
             return repository.save(newAluno);
         } else {
             throw new RuntimeException("Aluno with id: " + matricula + " not found");
